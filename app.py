df0c3ddaf885a1ba11b5bc9aa5c855c844b22e94
tahaziba99 <tahazibanaz@gmail.com>
2026-08-27 14:31:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "flask application running successfully"
+    return """
+    <html>
+    <body>
+    <h1>Hello RGUKT</h1>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
